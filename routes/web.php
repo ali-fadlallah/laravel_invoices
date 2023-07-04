4fdd109ce8915d432d504267c16c64c36a821284
Ali Fadlallah <alifadlallah355@gmail.com>
2023-07-04 05:16:30 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,11 @@ use App\Http\Controllers\InvoiceArchicesController;
 |
 */
 
+Route::get('/', function () {
+    // return view('welcome');
+
+    return view('auth.login');
+});
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
